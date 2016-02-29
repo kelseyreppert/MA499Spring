a275b27784b21093a2183851d896a467f0ff3c16
kelseyreppert <reppertkj@gmail.com>
2016-02-29 11:25:14 -0500

    Added files via upload

--- MA 499 Part 2/Approximate MHT/approximateMHT.m
+++ MA 499 Part 2/Approximate MHT/approximateMHT.m
@@ -1,26 +1,52 @@
 % To make things easier, this code will assume that there is a constant
 % numebr of measurements at each scan for all k
 
-[x1, z1, xbar1, H1, B1, scans1] = dataGeneration(0, 0, 1, pi/4, 1);
-[x2, z2, xbar2, H2, B2, scans2] = dataGeneration(10, 0, 1, 3*pi/4, 1);
+% Initial conditions and data generation
+x10 = 0;
+y10 = 0;
+speed1 = 1;
+angle1 = pi/4;
+radius1 = 1;
+[x1, z1, Q1, R1, dt] = dataGeneration(x10, y10, speed1, angle1, radius1);
 
-if isequal(H1,H2)
-    H = H1;
-end
+x20 = 10;
+y20 = 10;
+speed2 = 1;
+angle2 = 3*pi/4;
+radius2 = 1;
+[x2, z2, Q2, R2, dt] = dataGeneration(x20, y20, speed2, angle2, radius2);
 
-if isequal(scans1,scans2)
-    scans = scans1;
-end
+% MHT variables
+[dim, scans] = size(x1);
+PD = 0.9;
+H1 = eye(dim);
+GAMMA1 = eye(dim);
+H2 = eye(dim);
+GAMMA2 = eye(dim);
 
-X = {num2cell(x1,1); num2cell(x2,1)}; % Measurements Cell Array
-Z = {num2cell(z1,1); num2cell(z2,1)}; % Measurements Cell Array
-Xbar = {num2cell(xbar1,1); num2cell(xbar2,1)}; % Measurements Cell Array
-V = {num2cell(B1,1); num2cell(B2,1)}; % Measurements Cell Array
+% Initializing the structure arrays
+Xstruct = struct('x1',x1, 'x2', x2);
+Zstruct = struct('z1',z1, 'z2', z2);
+Xbarstruct = struct('x1bar',zeros(dim,scans),'x2bar',zeros(dim,scans));
+Xhatstruct = struct('x1hat',zeros(dim,scans),'x2hat',zeros(dim,scans));
+Pbarstruct = struct('Pbar1',zeros(dim,dim,scans),'Pbar2',zeros(dim,dim,scans));
+Phatstruct = struct('Phat1',zeros(dim,dim,scans),'Phat2',zeros(dim,dim,scans));
+Bstruct = struct('B1',zeros(dim,dim,scans),'B2',zeros(dim,dim,scans));
+Kstruct = struct('K1',zeros(dim,dim,scans),'K2',zeros(dim,dim,scans));
 
-% Assuming size(x1) == size(x2)
-[dim, ~] = size(x1);
-
-% plot(x1(1,:),x1(2,:),'r',x2(1,:),x2(2,:),'b');
+% Initial conditions
+Xbarstruct.xbar1(:,1) = Zstruct.z1(:,1); 
+Xhatstruct.xhat1(:,1) = Zstruct.z1(:,1); 
+Pbarstruct.Pbar1(:,:,1) = eye(dim); 
+Phatstruct.Phat1(:,:,1) = eye(dim); 
+Bstruct.B1(:,:,1) = eye(dim); 
+Kstruct.K1(:,:,1) = eye(dim); 
+Xbarstruct.xbar2(:,1) = Zstruct.z2(:,1); 
+Xhatstruct.xhat2(:,1) = Zstruct.z2(:,1); 
+Pbarstruct.Pbar2(:,:,1) = eye(dim); 
+Phatstruct.Phat2(:,:,1) = eye(dim); 
+Bstruct.B2(:,:,1) = eye(dim); 
+Kstruct.K2(:,:,1) = eye(dim); 
 
 % Organizes the measurement data
 mps = 2; % measurementsPerScan
@@ -33,7 +59,7 @@ OMEGA = generateHyp(mps, CT);
 % Initializes the probability matrix
 P = zeros(size(OMEGA));
 disp(OMEGA);
-row = input('Which row of OMEGA is has probability 1? ');
+row = input('Which row of OMEGA has probability 1? ');
 P(row,mps) = 1;
 toDelete = find(P(:,end)==0);
 P(toDelete,:) = [];
@@ -47,7 +73,25 @@ Ps{1} = P;
 
 NT = linspace(mps+1,maxTargets,maxTargets-mps); % All the possible new targets
 
+Ntgt = zeros(scans);
+Ntgt(1) = mps;
+
 for k = 1:scans
+    PHI1 = [1 0 -dt*speed1*sin(Xstruct.x1(3,k)); 0 1 dt*speed1*cos(Xstruct.x1(3,k)); 0 0 1 - dt*speed1/radius1];
+    Xbarstruct.xbar1(:,k+1) = PHI1*Xhatstruct.xhat1(:,k);
+    Pbarstruct.Pbar(:,:,k+1) = PHI1*Phatstruct.Phat1(:,:,k)*PHI1' + GAMMA1*Q1*GAMMA1';
+    Xhatstruct.xhat(:,k+1) = Xbarstruct.xbar1(:,k+1) + Kstruct.K1(:,:,k+1)*(Zstruct.z1(:,k+1) - H1*Xbarstruct.xbar1(:,k+1));
+    Phatstruct.Phat(:,:,k+1) = Pbarstruct.Pbar1(:,:,k+1) - Pbarstruct.Pbar1(:,:,k+1)*H1'*inv(H1*Pbarstruct.Pbar1(:,:,k+1)*H1' + R1)*H1*Pbarstruct.Pbar1(:,:,k+1);
+    Kstruct.K1(:,:,k+1) = Phatstruct.Phat1(:,:,k+1)*H1'*inv(R1);
+    Bstruct.B1(:,:,k+1) = H1*Pbarstruct.Pbar1(:,:,k+1)*H1' + R1;
+    
+    PHI2 = [1 0 -dt*speed2*sin(Xstruct.x2(3,k)); 0 1 dt*speed2*cos(Xstruct.x2(3,k)); 0 0 1 - dt*speed2/radius2];
+    Xbarstruct.xbar2(:,k+1) = PHI2*Xhatstruct.xhat2(:,k);
+    Pbarstruct.Pbar(:,:,k+1) = PHI2*Phatstruct.Phat2(:,:,k)*PHI2' + GAMMA2*Q2*GAMMA2';
+    Xhatstruct.xhat(:,k+1) = Xbarstruct.xbar2(:,k+1) + Kstruct.K2(:,:,k+1)*(Zstruct.z2(:,k+1) - H2*Xbarstruct.xbar2(:,k+1));
+    Phatstruct.Phat(:,:,k+1) = Pbarstruct.Pbar2(:,:,k+1) - Pbarstruct.Pbar2(:,:,k+1)*H2'*inv(H2*Pbarstruct.Pbar2(:,:,k+1)*H2' + R2)*H2*Pbarstruct.Pbar2(:,:,k+1);
+    Kstruct.K2(:,:,k+1) = Phatstruct.Phat2(:,:,k+1)*H2'*inv(R2);
+    Bstruct.B2(:,:,k+1) = H2*Pbarstruct.Pbar2(:,:,k+1)*H2' + R2;
     
     [OMEGA, P] = expandHyp(OMEGA, P, mps, CT, NT(mps*(k-1)+1:mps*k));
     [r,c] = size(OMEGA);
@@ -70,7 +114,7 @@ for k = 1:scans
         for j = 1:DT
            % if OMEGA(i+1,indA+j-1)
                 
-            product = product*mvnpdf(z(:,k+1), H*Xbar{1}{k+1}, B(:,:,k+1)); %%% NOTE: this should be z_j?
+            product = product*mvnpdf(z(:,k+1), H*Xbarcell{1}{k+1}, B(:,:,k+1)); %%% NOTE: this should be z_j?
         end
         P(i,indB) = PD^Ndt(i)*(1-PD)^(Ntgt(k) - Ndt(i))*betaFT^Nft(i)*betaNT^Nnt(i)*product*P(i,indA); % (16) Reid
     end
--- MA 499 Part 2/Approximate MHT/dataGeneration.m
+++ MA 499 Part 2/Approximate MHT/dataGeneration.m
@@ -1,5 +1,5 @@
 
-function [x, z, xbar, H, B, scans] = dataGeneration(x0, y0, speed, angle, radius)
+function [x, z, Q, R, dt] = dataGeneration(x0, y0, speed, angle, radius)
 dim = 3;
 
 % KF variables
@@ -14,8 +14,6 @@ tf = 10.0;
 scans = (tf-t0)/dt;
 
 % Model Variables
-H = eye(dim);
-GAMMA = eye(dim);
 w = q.*randn(dim,scans+1);
 v = r.*randn(dim,scans+1);
 
@@ -23,20 +21,6 @@ x = zeros(dim,scans+1);
 x(:,1) = [x0 y0 angle];
 z = zeros(dim,scans+1);
 z(:,1) = x(:,1) + w(:,1);
-xbar = zeros(dim,scans+1);
-xbar(:,1) = z(:,1);
-xhat = zeros(dim,scans+1);
-xhat(:,1) = z(:,1);
-Pbar = zeros(dim,dim,scans+1);
-Pbar(:,:,1) = eye(dim);
-Phat = zeros(dim,dim,scans+1);
-Phat(:,:,1) = eye(dim);
-K = zeros(dim,dim,scans+1);
-K(:,:,1) = eye(dim);
-B = zeros(dim,dim,scans+1);
-B(:,:,1) = eye(dim);
-
-
 
     function out = f(x,u,speed,radius,dt)
         out = x + dt*[speed*cos(x(3)) speed*sin(x(3)) speed*(u(3) - x(3))/radius]';
@@ -54,12 +38,6 @@ for k = 1:scans
     PHI = [1 0 -dt*speed*sin(x(3,k)); 0 1 dt*speed*cos(x(3,k)); 0 0 1 - dt*speed/radius];
     x(:,k+1) = f(x(:,k),u,speed,radius,dt) + w(:,k+1);
     z(:,k+1) = h(x(:,k+1)) + v(:,k+1);
-    xbar(:,k+1) = PHI*xhat(:,k);
-    Pbar(:,:,k+1) = PHI*Phat(:,:,k)*PHI' + GAMMA*Q*GAMMA';
-    xhat(:,k+1) = xbar(k+1) + K(:,:,k+1)*(z(:,k+1) - H*xbar(:,k+1));
-    Phat(:,:,k+1) = Pbar(:,:,k+1) - Pbar(:,:,k+1)*H'*inv(H*Pbar(:,:,k+1)*H' + R)*H*Pbar(:,:,k+1);
-    K(:,:,k+1) = Phat(:,:,k+1)*H'*inv(R);
-    B(:,:,k+1) = H*Pbar(:,:,k+1)*H' + R;
     
 end
 
